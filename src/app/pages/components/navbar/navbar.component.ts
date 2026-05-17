c365f922c04602dc0014860d08c19c8b3c82a81e
unknown <sherif.helmy@qeema.net>
2026-05-17 21:17:22 +0300

    finish the design of the navbar

--- src/app/pages/components/navbar/navbar.component.ts
+++ src/app/pages/components/navbar/navbar.component.ts
@@ -9,10 +9,12 @@ import {
   signal,
   ViewChild,
 } from '@angular/core';
+import { TabsModule } from 'primeng/tabs';
 import { SharedService } from '../../../components/services/shared.service';
 import { TranslationService } from '../../../components/services/translation.service';
 import { CommonModule } from '@angular/common';
 import { TranslatePipe } from '@ngx-translate/core';
+import { RouterLink } from '@angular/router';
 interface Language {
   arabicName: string;
   englishName: string;
@@ -25,9 +27,24 @@ interface CartItem {
   quantity: number;
   image: string;
 }
+interface CartItem {
+  id: number;
+  name: string;
+  price: number;
+  quantity: number;
+  image: string;
+}
+interface Notification {
+  id: number;
+  image: string;
+  text: string;
+  date: Date;
+  imageType: 'icon' | 'avatar';
+  username?: string;
+}
 @Component({
   selector: 'app-navbar',
-  imports: [CommonModule, TranslatePipe],
+  imports: [CommonModule, TranslatePipe, TabsModule, RouterLink],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
@@ -39,12 +56,17 @@ export class NavbarComponent {
   currentLanguage: InputSignal<'en' | 'ar'> = input.required<'en' | 'ar'>();
   private sharedService = inject(SharedService);
   currentLang!: Language;
+  isFullScreen = false;
   isLangDropdownOpen = signal(false);
   isCartDropdownOpen = signal(false);
   isSearchDropdownOpen = signal(false);
+  isNotificationsDropdownOpen = signal(false);
+  isUserDropdownOpen = signal(false);
   @ViewChild('langDropdown') langDropdown!: ElementRef;
   @ViewChild('cartDropdown') cartDropdown!: ElementRef;
   @ViewChild('searchDropdown') searchDropdown!: ElementRef;
+  @ViewChild('notificationsDropdown') notificationsDropdown!: ElementRef;
+  @ViewChild('userDropdown') userDropdown!: ElementRef;
   private translationService = inject(TranslationService);
   languages: Language[] = [
     {
@@ -95,14 +117,116 @@ export class NavbarComponent {
       image: 'images/5.png',
     },
   ];
+  notifications: Notification[] = [
+    {
+      id: 1,
+      image: 'ri-verified-badge-line',
+      text: '<b>Elite</b>Your  author Graphic Optimization <span class="text-secondary">reward</span> is ready!',
+      date: new Date('2023-06-01T10:30:00'),
+      imageType: 'icon',
+    },
+    {
+      id: 2,
+      image: 'images/img1.jpg',
+      text: "Answered to your comment on the cash flow forecast's graph 🔔.",
+      date: new Date('2025-03-15T10:30:00'),
+      imageType: 'avatar',
+      username: 'Angela Bernier',
+    },
+    {
+      id: 3,
+      image: 'ri-message-2-line',
+      text: 'You have received <b class="text-success">20</b> new messages in the conversation',
+      date: new Date('2025-07-05T10:30:00'),
+      imageType: 'icon',
+    },
+    {
+      id: 4,
+      image: 'images/img2.jpg',
+      text: '<b>Jacob Jones</b> mentioned you in a comment: <span class="text-secondary">"The new dashboard design looks great!"</span>',
+      date: new Date('2025-08-20T10:30:00'),
+      imageType: 'avatar',
+      username: 'Jacob Jones',
+    },
+  ];
+  UsersNotifications: Notification[] = [
+    {
+      id: 1,
+      image: 'images/img1.jpg',
+      text: "Answered to your comment on the cash flow forecast's graph 🔔.",
+      date: new Date('2025-03-15T10:30:00'),
+      imageType: 'avatar',
+      username: 'Angela Bernier',
+    },
+    {
+      id: 2,
+      image: 'images/img2.jpg',
+      text: '<b>Jacob Jones</b> mentioned you in a comment: <span class="text-secondary">"The new dashboard design looks great!"</span>',
+      date: new Date('2025-08-20T10:30:00'),
+      imageType: 'avatar',
+      username: 'Jacob Jones',
+    },
+  ];
+  // make function to convert date to time ago format
+  timeAgo(date: Date) {
+    const now = new Date();
+    const secondsAgo = Math.floor((now.getTime() - date.getTime()) / 1000);
+    if (secondsAgo < 60) {
+      return `${secondsAgo} seconds ago`;
+    } else if (secondsAgo < 3600) {
+      const minutesAgo = Math.floor(secondsAgo / 60);
+      return `${minutesAgo} minutes ago`;
+    } else if (secondsAgo < 86400) {
+      const hoursAgo = Math.floor(secondsAgo / 3600);
+      return `${hoursAgo} hours ago`;
+    } else if (secondsAgo < 2592000) {
+      const daysAgo = Math.floor(secondsAgo / 86400);
+      return `${daysAgo} days ago`;
+    } else if (secondsAgo < 31536000) {
+      const monthsAgo = Math.floor(secondsAgo / 2592000);
+      return `${monthsAgo} months ago`;
+    } else {
+      const yearsAgo = Math.floor(secondsAgo / 31536000);
+      return `${yearsAgo} years ago`;
+    }
+  }
   toggleLangDropdown() {
     this.isLangDropdownOpen.update((prev) => !prev);
+  }
+  toggleUserDropdown() {
+    this.isUserDropdownOpen.update((prev) => !prev);
   }
   toggleCartDropdown() {
     this.isCartDropdownOpen.update((prev) => !prev);
   }
+  toggleNotificationsDropdown() {
+    this.isNotificationsDropdownOpen.update((prev) => !prev);
+  }
   toggleSearchDropdown() {
     this.isSearchDropdownOpen.update((prev) => !prev);
+  }
+  @HostListener('document:fullscreenchange')
+  onFullscreenChange() {
+    this.isFullScreen = !!document.fullscreenElement;
+  }
+  toggleFullScreen() {
+    if (!this.isFullScreen) {
+      document.documentElement.requestFullscreen();
+    } else {
+      if (document.fullscreenElement) {
+        document.exitFullscreen();
+      }
+    }
+    this.isFullScreen = !this.isFullScreen;
+  }
+  removeCartItem(itemId: number) {
+    this.cartItems = this.cartItems.filter((item) => item.id !== itemId);
+  }
+  get totalPrice() {
+    return this.cartItems.reduce(
+      (total, item) => total + item.price * item.quantity,
+      0,
+    );
   }
   @HostListener('document:click', ['$event'])
   onClickOutside(event: Event) {
@@ -126,6 +250,20 @@ export class NavbarComponent {
         this.searchDropdown.nativeElement.contains(event.target);
       if (!clickedInsideSearchDropdown) {
         this.isSearchDropdownOpen.set(false);
+      }
+    }
+    if (this.notificationsDropdown) {
+      const clickedInsideNotificationsDropdown =
+        this.notificationsDropdown.nativeElement.contains(event.target);
+      if (!clickedInsideNotificationsDropdown) {
+        this.isNotificationsDropdownOpen.set(false);
+      }
+    }
+    if (this.userDropdown) {
+      const clickedInsideUserDropdown =
+        this.userDropdown.nativeElement.contains(event.target);
+      if (!clickedInsideUserDropdown) {
+        this.isUserDropdownOpen.set(false);
       }
     }
   }
